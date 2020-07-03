ed31f61f45a27624885d7d4b0db4bd4676d7baf1
Dang Pham <thienvu453@gmail.com>
2020-07-03 11:23:20 +0700

    project init

---
+++ src/main/java/com/SecurityWebApplicationInit.java
@@ -0,0 +1,6 @@
+package com;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInit extends AbstractSecurityWebApplicationInitializer {
+}
